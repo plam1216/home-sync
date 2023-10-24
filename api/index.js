b5cbc9600eed3f89f26edaee70295ec41e34e384
Peter Lam <plam1216@gmail.com>
2023-10-24 18:45:28 -0400

    connect to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express();
 const PORT = 3000
+
+// connect to MongoDB
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch(() => {
+        console.log(err)
+    })
+
 
 app.listen(PORT, () => {
     console.log('Listening on PORT', PORT)
